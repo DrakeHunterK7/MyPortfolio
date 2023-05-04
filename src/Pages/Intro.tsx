f5ccb4f16575460703ad4e27749cb13aeae5f231
Anas Peerzada <apeerzada987@gmail.com>
2023-05-04 02:50:26 -0400

    Massively improved look for the Intro page
    - smooth image transitions
    - smooth rotating gradient border on my photo

--- src/Pages/Intro.tsx
+++ src/Pages/Intro.tsx
@@ -3,34 +3,47 @@ import React, { useState } from 'react';
 import './Styles/Intro.scss';
 import { Button } from 'react-bootstrap';
 import selfie from '../Assets/latest-front-photo.jpg';
-import masseffect from '../Assets/gamedev-collage2.png';
+import gamedevImage from '../Assets/gamedev-collage2.png';
+import soenImage from '../Assets/soen-collage.png';
 
 function Intro() {
 
-  const images = ["none", masseffect]
+  const images = ["none", gamedevImage]
+  var [showImage, setShowImage] = React.useState(false)
   var [bgImage, setBgImage] = useState(images[0])
+  var [bgImageClass1, setBgImageClass1] = useState("imgFadeOut")
+  var [bgImageClass2, setBgImageClass2] = useState("imgFadeOut")
+  // var [bgOpacity, setBgOpacity] = useState(0.0)
+
+
+  function changeOpacity(direction: Number){
+    
+  }
 
   return (
     <div className="Intro">
       <header 
-        className="Intro-header fade-in"
-        style={{
-          backgroundImage: `url(${bgImage})`
-        }}>
+        className="Intro-header fade-in">
+          <img className={`bgImage ${bgImageClass1}`} alt="Background" src={gamedevImage}/>
+          <img className={`bgImageSmaller ${bgImageClass2}`} alt="Background" src={soenImage}/>
           <div className="Intro-selfie">
             <img alt="it me" src={selfie}/>
           </div>
 
           <div className="Intro-text">
             <h1>Hello! I am Anas. Welcome to my portfolio website!</h1>
-            <h2>Where would you like to go?</h2>
           </div>
 
           <div className="Intro-buttons">
-            <Button variant="primary" className="soen-button">Software Engineer Portfolio</Button>
+
+            <Button variant="primary" className="soen-button"
+            onMouseEnter={() => setBgImageClass2("imgFadeIn")}
+            onMouseLeave={() => setBgImageClass2("imgFadeOut")}
+            >Software Engineer Portfolio</Button>
+
             <Button variant="primary" className="gamedev-button" 
-            onMouseEnter={() => setBgImage(images[1])}
-            onMouseLeave={() => setBgImage(images[0])}>Game Programmer Portfolio</Button>
+            onMouseEnter={() => setBgImageClass1("imgFadeIn")}
+            onMouseLeave={() => setBgImageClass1("imgFadeOut")}>Game Programmer Portfolio</Button>
           </div>
       </header>
     </div>

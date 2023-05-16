4aa65e3e5ad43fe40158da08152e2a7861b59f34
Anas Peerzada <apeerzada987@gmail.com>
2023-05-16 12:51:55 -0400

    Added APCube and tab customization

--- src/Pages/HomePage.tsx
+++ src/Pages/HomePage.tsx
@@ -2,14 +2,20 @@
 import './Styles/HomePage.scss';
 import { useState } from "react";
 import { useMatch } from "react-router-dom";
+import { Suspense } from "react";
+
+import { Canvas } from "@react-three/fiber";
+import { Environment, OrbitControls } from "@react-three/drei";
+import { Model } from "./../Assets/Apcube";
 
 import Tab from 'react-bootstrap/Tab';
 import Tabs from 'react-bootstrap/Tabs';
 
+
 function HomePage() {
 
   const match = useMatch('portfolio/:portfolioType');
-  var [ pType, setPType ] = useState(match?.params.portfolioType);
+  var [ pType ] = useState(match?.params.portfolioType);
 
   function portfolioTitle() {
     if(pType === 'game-programmer')
@@ -20,22 +26,46 @@ function HomePage() {
 
   return (
     <div className="HomePage">
+
         <div className={pType+"-header"}>
-            <h1>Anas Peerzada</h1>
-            <h3>{portfolioTitle()} Portfolio</h3>
+            <div className="model-container">
+            <Canvas camera={{ fov: 90, zoom: 4 }}>
+                  <ambientLight intensity={1.25}/>
+                  <Suspense fallback={null}>
+                    <Model />
+                  </Suspense>
+                  <OrbitControls enableZoom={false} enablePan={false}/>
+                  <Environment preset="sunset" />
+                </Canvas>
+            </div>
+            <div className="header-title">
+              <h1>Anas Peerzada</h1>
+              <h3>{portfolioTitle()} Portfolio</h3>
+            </div>
         </div>
-        <Tabs
-              defaultActiveKey="about-me"
-              className={pType+"-tabs"}
-              justify
-            >
-              <Tab eventKey="about-me" title="About Me">
-              </Tab>
-              <Tab eventKey="projects" title="Projects and Experience">
-              </Tab>
-              <Tab eventKey="skills" title="Skills ">
-              </Tab>
-        </Tabs>
+
+        <div className={pType+"-body"}>
+          <Tabs
+            defaultActiveKey="about-me"
+            className={pType+"-tabs"}
+            justify
+          >
+            <Tab tabClassName={"tab"} eventKey="about-me" title="About Me">
+              <div className="tab-content-container">
+              </div>
+            </Tab>
+            <Tab tabClassName={pType+"-tab"} eventKey="projects" title="Projects and Experience">
+              <div className="tab-content-container">
+
+              </div>
+            </Tab>
+            <Tab tabClassName={pType+"-tab"} eventKey="skills" title="Skills ">
+              <div className="tab-content-container">
+
+              </div>
+            </Tab>
+          </Tabs>
+        </div>
     </div>
   );
 }

eb94bf3aa9e870b6ea2f404139f846bbd7c9e7fe
Anas Peerzada <apeerzada987@gmail.com>
2023-12-27 19:51:20 -0500

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -2,21 +2,18 @@ import ReactDOM from 'react-dom/client';
 import './index.scss';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { HashRouter, Routes, Route, Navigate } from "react-router-dom"
+// import { HashRouter, Routes, Route, Navigate } from "react-router-dom"
 import HomePage from 'Pages/HomePage';
-import ProjectPage from 'Pages/ProjectPage'
+// import ProjectPage from 'Pages/ProjectPage'
+import React from 'react';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <HashRouter>
-    <Routes>
-      <Route index path="/:portfolioType" element={<HomePage />} />
-      <Route index path="/:portfolioType/:projectName" element={<ProjectPage />}/>
-      <Route path="*" element= {<Navigate to='/game-programmer' replace />} />
-    </Routes>
-  </HashRouter>
+  <React.StrictMode>
+    <HomePage/>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
